2e533411cd8af28f17ab3b1460caa6ad0d84b08b
binhu <Bhu0002@conestogac.on.ca>
2024-12-12 22:27:49 -0500

    test

--- data-source.ts
+++ data-source.ts
@@ -40,4 +40,12 @@ const AppDataSource = new DataSource({
 
 console.log("TypeORM DataSource initialized");
 
+console.log({
+  DATABASE_HOST: process.env.DATABASE_HOST,
+  DATABASE_PORT: process.env.DATABASE_PORT,
+  DATABASE_USER: process.env.DATABASE_USER,
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+});
+
 export default AppDataSource;
